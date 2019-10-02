341a86978811d7d3b88b19cfed1d3f63ec17ccf4
Wahyudin <yudevan@yahoo.com>
2019-10-02 07:50:35 +0000

    Added vessel_map

--- Px/__init__.py
+++ Px/__init__.py
@@ -1,5 +1,9 @@
 __version__ = '1.0'
 __author__ = 'Wahyudin Aziz <yudevan@yahoo.com>'
+
+from plib import PLib
+
 __all__ = [
     'PLib',
 ]
+
--- Px/plib.py
+++ Px/plib.py
@@ -1,3 +1,4 @@
 class PLib(object):
     def __init__(self):
         super(PLib, self).__init__()
+        print self.__class__.__name__
